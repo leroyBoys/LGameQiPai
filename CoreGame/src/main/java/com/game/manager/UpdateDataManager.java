e63271b0d01bcbd3a507ff7e8eb06d2725775298
leroyBoys <wlvxiaohui@163.com>
2017-05-10 19:37:32 +0800

    chushihua222222222

--- CoreGame/src/main/java/com/game/manager/UpdateDataManager.java
+++ CoreGame/src/main/java/com/game/manager/UpdateDataManager.java
@@ -1,5 +1,7 @@
 package com.game.manager;
 
+import com.game.socket.module.UserVistor;
+
 import java.util.HashSet;
 import java.util.Set;
 /**
@@ -14,6 +16,7 @@ public class UpdateDataManager implements Runnable{
     }
 
     private UpdateDataManager() {
+        new Thread(this).start();
     }
 
     private Set<Integer> updateDataRoleIds = new HashSet<>();
@@ -22,9 +25,33 @@ public class UpdateDataManager implements Runnable{
         updateDataRoleIds.add(uid);
     }
 
+    private synchronized Set<Integer> resetUpdate(){
+        Set<Integer> tmp = new HashSet<>();
+        Set<Integer> ret = updateDataRoleIds;
+        updateDataRoleIds = tmp;
+        return ret;
+    }
 
     @Override
     public void run() {
-
+        while (true){
+            Set<Integer> ret = resetUpdate();
+            if(ret.isEmpty()){
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                continue;
+            }
+            UserVistor vistor;
+            for(Integer roleId:ret){
+                vistor =  OnlineManager.getIntance().getUserById(roleId);
+                if(vistor == null ||  vistor.getGameRole() == null){
+                    continue;
+                }
+                vistor.getGameRole().update();
+            }
+        }
     }
 }
